005377bb6a72c5e869c7b4b9d36c2df97beb4961
Abhishek-kushwaha <akushwaha@q3tech.com>
2014-01-09 11:19:04 +0530

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Recca Hanabhishi"
+    email    "recca@hokage.com"
+    password "flamedragons"
+    password_confirmation "flamedragons"
+  end
+end
