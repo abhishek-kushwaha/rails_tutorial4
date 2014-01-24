d9576e31d1355aace098ba2580106fc3ebecc4ac
Abhishek-kushwaha <akushwaha@q3tech.com>
2014-01-24 18:38:51 +0530

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RailsTutorial4::Application.routes.draw do
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   #get "users/new"
 
   root 'static_pages#home'
